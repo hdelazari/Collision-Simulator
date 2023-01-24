5cd3ffbf7f1a714df5586e42fd54af803fe0408b
Guilherme Lazari <guilhermelcs@gmail.com>
2023-01-24 22:45:29 +0000

    Open a display

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,25 @@
-#include<iostream>
+#include <X11/Xlib.h>
+#include <iostream>
 
-int main(){
-	std::cout << "Hello hannah\n";
-	std::cout << "Hello gui\n";
-	return 0;
+int main() {
+  Display *d = XOpenDisplay(NULL);
+  if (d == NULL) {
+    std::cout << "Unable to open the display\n";
+  }
+
+  int s = DefaultScreen(d);
+
+  Window w = XCreateSimpleWindow(d, RootWindow(d, s), 0, 0, 100, 100, 1,
+                                 BlackPixel(d, s), WhitePixel(d, s));
+
+  XMapWindow(d, w);
+
+
+  XPending(d);
+
+
+  getchar();
+
+  std::cout << "Exiting!!\n";
+  return 0;
 }
